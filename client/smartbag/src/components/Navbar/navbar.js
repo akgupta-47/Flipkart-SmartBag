ba9460448f4e8ef676ba5d1bbad91930750413cb
anm0l555 <anmoltiwari555@gmail.com>
2021-09-20 23:43:04 +0530

    Images folder added

--- client/smartbag/src/components/Navbar/navbar.js
+++ client/smartbag/src/components/Navbar/navbar.js
@@ -5,7 +5,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 function Navbar(){
    
     return(
-        <div classNameName="navbar">
+        <div classNameName="navbar center">
             <ul id="dropdown1" className="dropdown-content">
                 <li><a href="#!">one</a></li>
                 <li><a href="#!">two</a></li>
@@ -13,30 +13,29 @@ function Navbar(){
                 <li><a href="#!">three</a></li>
             </ul>
 
-            <nav className="nav-extended">
+            <nav className="nav-extended ">
                 <div className="nav-wrapper">
-                <a href="#!" className="brand-logo"><i>Grocery</i></a>
-
-                <ul className="right hide-on-med-and-down">
-                    <li>
-                        <form>
-                            <div className="input-field">
-                                <input id="search" type="search" required/>
-                                <label className="label-icon" for="search"><i className="material-icons">search</i></label>
-                                <i className="material-icons">close</i>
-                            </div>
-                        </form>
-                    </li>
-                    <li><a className="dropdown-trigger" href="#" data-target="dropdown1">A link<i class="material-icons right">arrow_drop_down</i></a></li>
-                    <li><a className="dropdown-trigger" href="#" data-target="dropdown1">A second link<i class="material-icons right">arrow_drop_down</i></a></li>
-                    <li><a className="dropdown-trigger" href="#" data-target="dropdown1">A third link<i class="material-icons right">arrow_drop_down</i></a></li>
-                </ul>
+                    <a href="#!" className="heading "><i>Grocery</i></a>
+                    <ul className=" hide-on-med-and-down" >
+                        
+                        <li>
+                            <form>
+                                <div className="input-field white black-text">
+                                    <input id="search" className="black" placeholder="Search here.." type="search" required/>
+                                    <label className="label-icon" for="search"><i className="material-icons">search</i></label>
+                                    <i className="material-icons">close</i>
+                                </div>
+                            </form>
+                        </li>
+                        <li><a className="dropdown-trigger" href="#" data-target="dropdown1">A link<i class="material-icons right">arrow_drop_down</i></a></li>
+                        <li><a className="dropdown-trigger" href="#" data-target="dropdown1">A second link<i class="material-icons right">arrow_drop_down</i></a></li>
+                        <li><a className="dropdown-trigger" href="#" data-target="dropdown1">A third link<i class="material-icons right">arrow_drop_down</i></a></li>
+                    </ul>
                 </div>
-                <div className="nav-content">
-                <span className="nav-title">Title</span>
-                <a className="btn-floating btn-large halfway-fab waves-effect waves-light teal">
-                    <i className="material-icons">add</i>
-                </a>
+                <div className="nav-content white">
+                    {/* <img src="" */}
+
+                
                 </div>
             </nav>
         </div>
