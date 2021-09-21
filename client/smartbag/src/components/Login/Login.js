92f22bcde53a03ce4be3eebe73506fd76d484ad5
Snehil-Mittal <snehilmittal2001@gmail.com>
2021-09-21 15:16:56 +0530

    Login

--- client/smartbag/src/components/Login/Login.js
+++ client/smartbag/src/components/Login/Login.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import 'materialize-css/dist/css/materialize.min.css';
+import React, {useState}from 'react'
 import './Login.css'
+import 'materialize-css/dist/css/materialize.min.css';
+
 const Login =()=>{
     return(
         <div className="login">
@@ -22,11 +23,11 @@ const Login =()=>{
                             <div class="input-field col s12">
                                 <input type="text" id="autocomplete-input2" class="autocomplete label-input"/>
                                  <label for="autocomplete-input2">Enter Password</label>
-                                 <p className="p-lower">By continuing, you agree to <span className="blue-text">Flipkart's Terms of Use</span> and <span className="blue-text">Privacy Policy.</span></p>
+                                 <p className="p-lower">By continuing, you agree to Flipkart's <a href="#terms" className="blue-text alower">Terms of Use</a> and <a href="#policy"className="blue-text alower">Privacy Policy.</a></p>
                                  <a class="login-btn btn">Login</a>
                                  <p className="p-lower2">OR</p>
-                                 <a class="otp-btn btn z-depth-4">Request OTP</a>
-                                 <p className="p-lower3 center blue-text">New to Flipkart? Create an account</p>
+                                 <a class="otp-btn btn">Request OTP</a>
+                                 <a href="#signup"className="p-lower3 center blue-text alower3">New to Flipkart? Create an account</a>
                             </div>
                             </div>
                         </div>
