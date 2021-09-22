9c578ff270011fc2ba65e1635632c7c4b3d351a8
Snehil-Mittal <snehilmittal2001@gmail.com>
2021-09-22 22:19:12 +0530

    Signup added

--- client/smartbag/src/components/Login/Login.js
+++ client/smartbag/src/components/Login/Login.js
@@ -5,11 +5,23 @@ import 'materialize-css/dist/css/materialize.min.css';
 const Login = () => {
   const [credentials, setCredentials] = useState('');
   const [password, setPassword] = useState('');
-  
+  const [newCredentials, setNewCredentials] = useState('');
+
   const handleLogin = (e) => {
     e.preventDefault();
     console.log(credentials);
     console.log(password);
+  };
+
+  const handleOTP = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+    console.log(password);
+  };
+
+  const handleNewLogin = (e) => {
+    e.preventDefault();
+    console.log(newCredentials);
   };
 
   return (
@@ -55,7 +67,7 @@ const Login = () => {
                   />
                   <label for="autocomplete-input2">Enter Password</label>
                   <p className="p-lower">
-                    By continuing, you agree to Flipkart's
+                    By continuing, you agree to Flipkart's{' '}
                     <a href="#terms" className="blue-text alower">
                       Terms of Use
                     </a>{' '}
@@ -64,16 +76,66 @@ const Login = () => {
                       Privacy Policy.
                     </a>
                   </p>
-                  <a class="login-btn btn" onClick={handleLogin}>
+                  <a class="login-btn btn" href="#login" onClick={handleLogin}>
                     Login
                   </a>
                   <p className="p-lower2">OR</p>
-                  <a class="otp-btn btn">Request OTP</a>
+                  <a class="otp-btn btn" href="#otp" onClick={handleOTP}>
+                    Request OTP
+                  </a>
                   <a
                     href="#signup"
-                    className="p-lower3 center blue-text alower3"
+                    className="p-lower3 center blue-text alower3 modal-trigger"
                   >
                     New to Flipkart? Create an account
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div id="signup" class="modal">
+        <div class="modal-content">
+          <div className="row">
+            <div className="col left-col s5">
+              <h4>Looks like you're new here!</h4>
+              <p>Sign up with your mobile number to get started</p>
+              <div className="signup_img"></div>
+            </div>
+            <div className="col center right-col2 s7">
+              <div className="row">
+                <div class="input-field col s12">
+                  <input
+                    type="text"
+                    id="autocomplete-input3"
+                    class="autocomplete label-input"
+                    value={newCredentials}
+                    onChange={(e) => {
+                      setNewCredentials(e.target.value);
+                    }}
+                    required
+                  />
+                  <label for="autocomplete-input3">
+                    Enter your Mobile Number
+                  </label>
+
+                  <p className="ps-lower">
+                    By continuing, you agree to Flipkart's{' '}
+                    <a href="#terms" className="blue-text alower">
+                      Terms of Use
+                    </a>{' '}
+                    and{' '}
+                    <a href="#policy" className="blue-text alower">
+                      Privacy Policy.
+                    </a>
+                  </p>
+                  <a class="login-btn2 btn" onClick={handleNewLogin}>
+                    Continue
+                  </a>
+                  <a class="otp-btn2 btn modal-close" href="#!">
+                    Existing User? Log in
                   </a>
                 </div>
               </div>
